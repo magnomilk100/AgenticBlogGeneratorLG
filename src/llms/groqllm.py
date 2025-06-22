1fc7c5a0fbea33fc7dbf4b0cd24f89e00b2b936c
Magno Leite <m.leite@avintis.com>
2025-06-22 09:55:05 +0200

    Blog ready with no Language choice support

---
+++ src/llms/groqllm.py
@@ -0,0 +1,19 @@
+from langchain_groq import ChatGroq
+import os 
+from dotenv import load_dotenv
+
+class GroqLLM:
+    def __init__(self):
+        load_dotenv()
+
+
+    def get_llm(self):
+        try:
+            print(os.getenv("GROQ_API_KEY"))
+            os.environ["GROQ_API_KEY"]=self.groq_api_key=os.getenv("GROQ_API_KEY")
+            if not self.groq_api_key:
+                raise ValueError("GROQ_API_KEY is missing — did you create the .env file?")                 
+            llm=ChatGroq(api_key=self.groq_api_key,model="llama-3.1-8b-instant")
+            return llm
+        except Exception as e:
+            raise ValueError("Error occurred with exception : {e}")            
